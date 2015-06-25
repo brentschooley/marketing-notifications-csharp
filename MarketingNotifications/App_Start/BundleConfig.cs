0413942ec768fb64a6236a06570b5232a399692e
brentschooley <schooleybrent@gmail.com>
2015-06-25 16:03:36 -0400

    Favicons and script tag addition.

--- MarketingNotifications/App_Start/BundleConfig.cs
+++ MarketingNotifications/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace MarketingNotifications
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
